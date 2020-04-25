ce44272442d59b064ebcf411c1ed70894004c22c
wisestMe <wyhzest@gmail.com>
2020-04-25 04:28:51 +0100

    add contacts array

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
 
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
+ 
 class App extends Component {
   render() {
     return (
