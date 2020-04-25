37474244f0d71b1466d2ae07d557b2c0c2093299
wisestMe <wyhzest@gmail.com>
2020-04-25 04:44:46 +0100

    create component

---
+++ src/ListContacts.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ListContacts extends Component {
+    render() {
+        return (
+            <ol className="contacts-list">
+
+            </ol>
+        )
+    }
+}
+
+export default ListContacts
